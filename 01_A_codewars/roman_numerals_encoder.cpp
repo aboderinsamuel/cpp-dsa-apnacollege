889eaaa15e5840456dbafac608f185847e095e13
Samuel Aboderin <aboderinseun01@gmail.com>
2026-02-13 00:15:05 +0100

    12AM 12th feb,26

---
+++ 01_A_codewars/roman_numerals_encoder.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+// This is a greedy problem
+using namespace std;
+string solution(int number) {
+    vector<pair<int, string>> symbols = {
+        {1000, "M"},
+    {900, "CM"},
+    {500, "D"},
+    {400, "CD"},
+    {100, "C"},
+    {90, "XC"},
+    {50, "L"},
+    {40, "XL"},
+    {10, "X"},
+    {9, "IX"},
+    {5, "V"},
+    {4, "IV"},
+    {1, "I"},
+    };
+    string result;
+    for (auto& [value, roman] : symbols) {
+        while (number >= value) {
+            result += roman; //start from the highest, change the highest denomenation to roman numeral
+            number -= value; // deduct the digit from current digit
+        }
+    }
+    return result;
+}
